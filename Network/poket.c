1193d31bf03e8125e6eac1ac83269d4b6c5a407b
Gor <158300420+Gorway@users.noreply.github.com>
2024-07-27 11:37:44 +0400

    Update poket.c

--- Network/poket.c
+++ Network/poket.c
@@ -17,13 +17,11 @@ int main() {
     char buffer[BUFFER_SIZE] = {0};
     const char *hello = "Hello from server";
 
-    // Создание серверного сокета
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
     
-    // Настройка опций сокета
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
@@ -33,25 +31,21 @@ int main() {
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
     
-    // Привязка сокета к адресу и порту
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
     
-    // Ожидание подключения
     if (listen(server_fd, MAX_PENDING_CONNECTIONS) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
     
-    // Принятие подключения от клиента
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
     
-    // Отправка данных клиенту
     send(new_socket, hello, strlen(hello), 0);
     printf("Hello message sent\n");
     return 0;
