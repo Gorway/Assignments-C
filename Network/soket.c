7b8659ad8ce04ebe23168cd2a290814b1f955cbe
Gor <158300420+Gorway@users.noreply.github.com>
2024-07-27 11:40:13 +0400

    Update soket.c

--- Network/soket.c
+++ Network/soket.c
@@ -27,56 +27,45 @@ int main() {
     int clilen = sizeof(cli_addr);
     char buffer[BUFFER_SIZE];
 
-    // Создание сокета
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // Настройка адреса сервера
     memset(&serv_addr, '0', sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(PORT);
 
-    // Привязка сокета к адресу и порту
     if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // Прослушивание на сокете
     if (listen(sockfd, 5) < 0) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
-    // Принятие подключения
     if ((newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, (socklen_t *)&clilen)) < 0) {
         perror("Accept failed");
         exit(EXIT_FAILURE);
     }
 
-    // Создание нового процесса
     if ((pid = fork()) < 0) {
         perror("Fork failed");
         exit(EXIT_FAILURE);
     }
 
-    // Потомок
     if (pid == 0) {
-        close(sockfd); // Закрытие дескриптора сокета
-        // Чтение из сокета
+        close(sockfd); 
         read(newsockfd, buffer, BUFFER_SIZE);
         printf("Received message: %s\n", buffer);
-        // Переворот строки
         reverseString(buffer);
-        // Отправка перевернутой строки обратно в сокет
         write(newsockfd, buffer, strlen(buffer));
         exit(0);
     } else { // Родитель
-        close(newsockfd); // Закрытие дескриптора нового сокета
-        // Ожидание завершения дочернего процесса
+        close(newsockfd); 
         wait(NULL);
     }
     return 0;
