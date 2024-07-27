916dafe24f8891a06afe17509a62e16f392843ea
Gor <158300420+Gorway@users.noreply.github.com>
2024-07-27 11:36:58 +0400

    Update client.c

--- Network/client.c
+++ Network/client.c
@@ -10,10 +10,9 @@
 int main() {
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Привет, это сообщение от клиента!";
+    char *hello = "Hello , message from client!";
     char buffer[BUFFER_SIZE] = {0};
 
-    // Создание сокета
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
         return -1;
@@ -22,21 +21,18 @@ int main() {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Преобразование IP адреса из текстового формата в структуру
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
 
-    // Подключение к серверу
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         printf("\nConnection Failed \n");
         return -1;
     }
 
-    // Отправка сообщения серверу
     send(sock , hello , strlen(hello) , 0 );
-    printf("Приветное сообщение отправлено\n");
+    printf("Greeting message sended\n");
     valread = read( sock , buffer, BUFFER_SIZE);
     printf("%s\n",buffer );
     return 0;
